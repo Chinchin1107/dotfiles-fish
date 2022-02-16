697674242820acfeb60b0885cbaeeefe799850f2
Chinchin <chinchin.anawach@gmail.com>
2022-02-16 17:03:47 +0700

    init

---
+++ config.fish
@@ -0,0 +1,70 @@
+set fish_greeting ""
+
+export PATH="(yarn global bin):$PATH"
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls ll
+alias ll 'exa -l -g --icons'
+alias lla "ll -a"
+alias llt "ll --tree"
+alias llts "llt --level=3"
+alias llta "llt -a"
+alias lltas "llta --level=3"
+
+alias g git
+
+alias cls clear
+alias clr clear
+
+alias tm tmux
+alias tmls 'tmux ls'
+alias tmks 'tmux kill-session'
+alias tmat 'tmux attach'
+
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+  status --is-command-substitution; and return
+
+  if test -f .nvmrc; and test -r .nvmrc;
+    nvm use
+  else
+  end
+end
+
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    source (dirname (status --current-filename))/config-linux.fish
+  case '*'
+    source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
+end
---
+++ readme.md
@@ -0,0 +1,21 @@
+# **Fish Shell** dotfiles
+## config fish shell
+---
+### **Before, Use it to install fish shell**
+```
+brew install fish
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+fisher install ilancosman/tide@v5
+
+(install nerd fonts)
+
+fisher install jethrokuan/z
+
+brew install exa
+
+brew install ghq
+
+brew install peco
+```
